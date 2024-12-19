9388555da557432642c2656e7427f3e84d576273
jretterhg <156801586+jretterhg@users.noreply.github.com>
2024-12-19 16:17:21 +0100

    Update player2.c

--- player2.c
+++ player2.c
@@ -57,6 +57,7 @@ void	update_old_position(t_game *game)
 int	handle_exit(t_game *game, int new_x, int new_y)
 {
 	char	target_tile;
+	char	current_tile;
 
 	current_tile = game->map[game->player_y][game->player_x];
 	target_tile = game->map[new_y][new_x];
